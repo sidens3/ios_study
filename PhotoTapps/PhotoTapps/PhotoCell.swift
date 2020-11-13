cd8d211f7ebbe652c53f2291db68f50217176de6
sidens3 <sidens3@gmail.com>
2020-11-13 10:43:26 +0300

    configure custom collectionView Cell

--- PhotoTapps/PhotoTapps/PhotoCell.swift
+++ PhotoTapps/PhotoTapps/PhotoCell.swift
@@ -9,4 +9,7 @@ import UIKit
 
 class PhotoCell: UICollectionViewCell {
     
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+    
 }
--- PhotoTapps/PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotoTapps/PhotosCollectionViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
 
-    let itemsPerRow: CGFloat = 2
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10",
+                  "dog11", "dog12", "dog13", "dog14", "dog15"]
+    let itemsPerRow: CGFloat = 3
     let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +37,16 @@ class PhotosCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-        cell.backgroundColor = .black
+        
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.dogImageView.image = image
+//        cell.backgroundColor = .black
     
         // Configure the cell
     
